28e622c83ebd4c7e07212e14edeb495d621659d5
Kesler Tanner <keslert@gmail.com>
2017-06-13 11:21:28 -0700

    paragraph lorem ipsum generation

--- src/core/generator/content/generate.js
+++ src/core/generator/content/generate.js
@@ -7,6 +7,8 @@ export function generateContent(element) {
   switch(element.name) {
     case 'BasicHeading':
       return {text: LoremIpsum({count: random(4, 8), units: 'words'})};
+    case 'BasicParagraph':
+      return {text: LoremIpsum({count: random(2, 4), units: 'sentences'})};
     case 'Button':
       return getButtonContent(element);
     case 'Paragraph':
