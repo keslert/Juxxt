23047d33de64a6aa5702cb24146b7af622d69dbe
Renata Drucker <renata.drucker@gmail.com>
2017-07-18 09:52:28 -0700

    space theme added for Naomi

--- src/core/generator/themes/index.js
+++ src/core/generator/themes/index.js
@@ -4,5 +4,6 @@ import TravelBlog from './travel-blog';
 import FashionHipster from './fashion-hipster';
 import HealthyFood from './healthy-food';
 import IceCreamShop from './ice-cream-shop';
+import Space from './space';
 
-export default IceCreamShop;
+export default Space;
