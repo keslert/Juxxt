28c7dbc2723795360bc49962412cdf2f8d208733
naomi789 <snjohnson789@gmail.com>
2017-08-04 13:57:15 -0700

    storing if fonts are uppercase transform ugly or not

--- src/core/generator/fonts.js
+++ src/core/generator/fonts.js
@@ -30,62 +30,82 @@ export const paragraphs = zipObject(paragraphNames, map(paragraphNames, name => 
 export const fontDetails = {
 	'Alice': {
 		weights: [400],
+		canUppercase: true,
 	},
 	'Amatic SC': {
 		weights: [400, 700],
+		canUppercase: false,
 	},
 	'Bitter': {
 		weights: [400, 700, '400-italic'],
+		canUppercase: true,
 	},
 	'Josefin Sans': {
 		weights: [100, 300, 400, 600, 700, '100-italic', '300-italic', '400-italic', '600-italic', '700-italic'],
+		canUppercase: true,
 	},
 	'La Belle Aurore': {
 		weights: [400],
+		canUppercase: true,
 	},
 	'Lato': {
 		weights: [100, 300, 400, 700, 900, '100-italic', '300-italic', '400-italic', '700-italic', '900-italic'],
+		canUppercase: true,
 	},
 	'Meddon': {
 		weights: [400],
+		canUppercase: false,
 	},
 	'Merriweather': {
 		weights: [300, 400, 700, 900, '300-italic', '400-italic', '700-italic', '900-italic'],
+		canUppercase: true,
 	},
 	'Montserrat': {
 		weights: [400, 700],
+		canUppercase: true,
 	},
 	'Muli': {
 		weights: [300, 400, '300-italic', '400-italic'],
+		canUppercase: true,
 	},
 	'Open Sans': {
 		weights: [300, 400, 600, 700, 900, '300-italic', '400-italic', '600-italic', '700-italic', '900-italic'],
+		canUppercase: true,
 	},
 	'Open Sans Condensed': {
 		weights: [300, 700, '300-italic'],
+		canUppercase: true,
 	},
 	'Oswald': {
 		weights: [300, 400, 700],
+		canUppercase: true,
 	},
 	'Playfair Display': {
 		weights: [400, 700, 900, '400-italic', '700-italic', '900-italic'],
+		canUppercase: true,
 	},
 	'Quicksand': {
 		weights: [300, 400, 700],
+		canUppercase: true,
 	},
 	'Raleway': {
 		weights: [100, 200, 300, 400, 500, 600, 700, 900],
+		canUppercase: true,
 	},
 	'Roboto': {
 		weights: [100, 300, 400, 500, 700, 900, '100-italic', '300-italic', '400-italic', '500-italic', '700-italic', '900-italic'],
+		canUppercase: true,
 	},
 	'Roboto Slab': {
 		weights: [100, 300, 400, 700],
+		canUppercase: true,
 	},
 	'Sacramento': {
 		weights: [400],
+		canUppercase: false,
 	},
 	'Work Sans': {
 		weights: [100, 200, 300, 400, 500, 600, 700, 900],
+		canUppercase: true,
 	},
 }
